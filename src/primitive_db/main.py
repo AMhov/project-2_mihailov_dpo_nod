3e25baba1fa27b885a16de803f37648a3334bf2a
Anton Mhov <mhov@yandex.ru>
2025-11-18 19:11:22 +0300

    pr2: Fix after test. Modul 2 complit. It works well.

--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -3,13 +3,11 @@
 from src.primitive_db.engine import welcome
 
 
-
-
 def main():
     """Основная функция"""
     print("DB project is running!\n")
 
     welcome()
 
-# if __name__ == "__main__":
-main()
+if __name__ == "__main__":
+    main()
--- src/primitive_db/util_read_config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-config_path = 'src/config.yaml'
-
-def read_config():
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-    return config
